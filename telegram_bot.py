0522afc390de6f3b639a29b0942089e1afd612ea
DavidRoqueAtienza <155072722+DavidRoqueAtienza@users.noreply.github.com>
2026-05-04 08:35:08 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,7 +8,6 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTyp
 # ===== CONFIG =====
 
 
-
 TELEGRAM_TOKEN = os.getenv("7696157057:AAEVHhM7HRdUHYHq_EExLJ3fu39D8qJkzr0")
 SMARTSHEET_TOKEN = os.getenv("M3vmRneTpdBhPuLqYQecdJKjNqNjY5eHR4meM")
 SHEET_ID = int(os.getenv("2162883748122500"))
